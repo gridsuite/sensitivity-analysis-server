0f8b1e0a7be6ab96e7ad865f754ce2400758648d
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-11-29 12:20:52 +0100

    Optimize result deletion by defining explicit query

---
+++ src/test/java/org/gridsuite/sensitivityanalysis/server/util/TestUtils.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.sensitivityanalysis.server.util;
+
+
+import static com.vladmihalcea.sql.SQLStatementCountValidator.*;
+
+/**
+ * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+ */
+public final class TestUtils {
+    public static void assertRequestsCount(long select, long insert, long update, long delete) {
+        assertSelectCount(select);
+        assertInsertCount(insert);
+        assertUpdateCount(update);
+        assertDeleteCount(delete);
+    }
+}
