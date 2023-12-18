df1535539b88e8e21b86a90a0a1c31d7f802991f
jamal-khey <myjamal89@gmail.com>
2023-12-18 11:57:19 +0100

    fix api path

--- src/main/java/org/gridsuite/sensitivityanalysis/server/service/FilterService.java
+++ src/main/java/org/gridsuite/sensitivityanalysis/server/service/FilterService.java
@@ -62,7 +62,7 @@ public class FilterService {
         Objects.requireNonNull(networkUuid);
 
         var uriComponentsBuilder = UriComponentsBuilder
-                .fromPath(DELIMITER + FILTER_API_VERSION + "/filters/exports")
+                .fromPath(DELIMITER + FILTER_API_VERSION + "/filters/export")
                 .queryParam(IDS, filterUuids)
                 .queryParam(NETWORK_UUID, networkUuid.toString());
         if (!StringUtils.isBlank(variantId)) {
