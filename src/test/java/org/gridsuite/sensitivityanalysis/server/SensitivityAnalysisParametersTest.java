7cf3bf024bf1edad5922ee5e8e524dd50f5f5700
Florent MILLOT <millotflo@gmail.com>
2024-01-31 18:30:26 +0100

    Update doc
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/test/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisParametersTest.java
+++ src/test/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisParametersTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -37,7 +37,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * @author Ayoub LABIDI <ayoub.labidi at rte-france.com>
+ * @author Florent MILLOT <florent.millot at rte-france.com>
  */
 @SpringBootTest
 @AutoConfigureMockMvc
--- src/test/java/org/gridsuite/sensitivityanalysis/server/util/assertions/Assertions.java
+++ src/test/java/org/gridsuite/sensitivityanalysis/server/util/assertions/Assertions.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -12,7 +12,7 @@ import org.gridsuite.sensitivityanalysis.server.dto.*;
 import org.gridsuite.sensitivityanalysis.server.dto.parameters.SensitivityAnalysisParametersInfos;
 
 /**
- * @author Tristan Chuine <tristan.chuine at rte-france.com>
+ * @author Florent MILLOT <florent.millot at rte-france.com>
  * {@link org.assertj.core.api.Assertions Assertions} completed with our custom assertions classes.
  */
 public class Assertions extends org.assertj.core.api.Assertions {
