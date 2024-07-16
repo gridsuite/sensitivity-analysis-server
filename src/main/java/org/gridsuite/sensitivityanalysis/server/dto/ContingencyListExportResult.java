4214ee5f8b91c27a9ca448b3a924beae0949e7c5
jamal-khey <myjamal89@gmail.com>
2024-07-16 14:30:27 +0200

    fix licence header

--- src/main/java/org/gridsuite/sensitivityanalysis/server/dto/ContingencyListExportResult.java
+++ src/main/java/org/gridsuite/sensitivityanalysis/server/dto/ContingencyListExportResult.java
@@ -1,11 +1,11 @@
-package org.gridsuite.sensitivityanalysis.server.dto;
-
-/**
+/*
  * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+package org.gridsuite.sensitivityanalysis.server.dto;
+
 import com.powsybl.contingency.Contingency;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
