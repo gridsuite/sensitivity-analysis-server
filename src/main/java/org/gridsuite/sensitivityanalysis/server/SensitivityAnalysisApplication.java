8db0ad2b08ace34edddc6fe798ce81d9ea73c972
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-11-27 16:54:53 +0100

    Rename handlers

--- src/main/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisApplication.java
+++ src/main/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisApplication.java
@@ -8,7 +8,7 @@ package org.gridsuite.sensitivityanalysis.server;
 
 import com.powsybl.network.store.client.NetworkStoreService;
 import com.powsybl.ws.commons.error.BaseExceptionHandler;
-import org.gridsuite.computation.error.ComputationRestResponseEntityExceptionHandler;
+import org.gridsuite.computation.error.ComputationExceptionHandler;
 import org.gridsuite.computation.service.NotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
  */
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-@SpringBootApplication(scanBasePackageClasses = {SensitivityAnalysisApplication.class, NetworkStoreService.class, NotificationService.class, ComputationRestResponseEntityExceptionHandler.class, BaseExceptionHandler.class})
+@SpringBootApplication(scanBasePackageClasses = {SensitivityAnalysisApplication.class, NetworkStoreService.class, NotificationService.class, ComputationExceptionHandler.class, BaseExceptionHandler.class})
 public class SensitivityAnalysisApplication {
     public static void main(String[] args) {
         SpringApplication.run(SensitivityAnalysisApplication.class, args);
