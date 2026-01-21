bded0356eecaa8090b789734463c7e0fead15e57
Jon Harper <jon.harper87@gmail.com>
2026-01-21 11:26:03 +0100

    refactor and fixes for SensitivityResultWriterPersistedTest (#152)
    To ensure batching, we should assert that the number of calls is < 3 to show easily that there are not
    1000 calls (=no batching). It's more explicit than relying on atMost(1000ms) together with sleep(250ms)
    in the doAnswers
    
    When writing a single value, we can verify exactly called once instead of atLeast
    
    Cleanup: don't call verify(0) at the begining of every test, it's redundant and verbose
    and only tests that we reset mocks but at some point we must stop testing the test...
    
    cleanup: fix minor copy/paste divergences in tests

--- src/test/java/org/gridsuite/sensitivityanalysis/server/util/SensitivityResultWriterPersistedTest.java
+++ src/test/java/org/gridsuite/sensitivityanalysis/server/util/SensitivityResultWriterPersistedTest.java
@@ -17,17 +17,19 @@ import org.mockito.Mockito;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
 /**
  * @author Joris Mancini <joris.mancini_externe at rte-france.com>
+ * @author Jon Schuhmacher <jon.harper at at rte-france.com>
  */
 class SensitivityResultWriterPersistedTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SensitivityResultWriterPersistedTest.class);
@@ -68,98 +70,122 @@ class SensitivityResultWriterPersistedTest {
         resultWriterPersisted.interrupt();
     }
 
+    private void testOperating(boolean started, boolean finished, boolean interrupted) throws InterruptedException {
+        if (started) {
+            resultWriterPersisted.start();
+        }
+        if (interrupted) {
+            resultWriterPersisted.interrupt();
+        }
+        resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.);
+        resultWriterPersisted.writeContingencyStatus(0, SensitivityAnalysisResult.Status.SUCCESS);
+        assertFalse(resultWriterPersisted.isConsumerFinished());
+        if (finished) {
+            resultWriterPersisted.setQueueProducerFinished();
+        }
+        // This test always uses Thread.sleep, even though when
+        // we start and either finish or interrupt we could just wait for isConsumerFinished()
+        // This is done to ensure that sleeping correctly exhibit the tested behavior in the cases
+        // where we can't wait for isConsumerFinished()
+        // This makes the test slow so don't overuse it.
+        // When we rewrite this test class to avoid real delays, we can improve this (for example
+        // using a countdownlatch to guarantee execution order around SensitivityAnalysisResultService
+        // method calls)
+        Thread.sleep(500);
+        assertEquals(started && (finished || interrupted), resultWriterPersisted.isConsumerFinished());
+        int times = started && !interrupted ? 1 : 0;
+        verify(analysisResultService, times(times)).writeSensitivityValues(any(), anyList());
+        verify(analysisResultService, times(times)).writeContingenciesStatus(any(), anyList());
+    }
+
     @Test
     void testNotOperatingIfNotStarted() throws InterruptedException {
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
-        resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.);
-        assertFalse(resultWriterPersisted.isConsumerFinished());
+        testOperating(false, false, false);
+    }
+
+    @Test
+    void testOperatingIfStartedNotFinished() throws InterruptedException {
+        testOperating(true, false, false);
+    }
+
+    @Test
+    void testOperatingIfStartedInterrupted() throws InterruptedException {
+        testOperating(true, false, true);
+    }
+
+    @Test
+    void testOperatingIfStartedFinished() throws InterruptedException {
+        testOperating(true, true, false);
+    }
+
+    private void testWritingValue(Runnable writeOne, Consumer<SensitivityAnalysisResultService> verify, boolean batched, boolean throwing) {
+        if (throwing) {
+            doThrow(new RuntimeException("Error persisting sensitivity values"))
+                .when(analysisResultService)
+                .writeSensitivityValues(any(), anyList());
+            doThrow(new RuntimeException("Error persisting contingency statuses"))
+                .when(analysisResultService)
+                .writeContingenciesStatus(any(), anyList());
+        }
+        resultWriterPersisted.start();
+        if (batched) {
+            IntStream.range(0, SensitivityResultWriterPersisted.BUFFER_SIZE * 3 / 2).forEach(i -> writeOne.run());
+        } else {
+            writeOne.run();
+        }
         resultWriterPersisted.setQueueProducerFinished();
-        Thread.sleep(500);
-        assertFalse(resultWriterPersisted.isConsumerFinished());
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
+        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
+        if (batched && !throwing) {
+            verify.accept(verify(analysisResultService, atLeast(2)));
+            verify.accept(verify(analysisResultService, atMost(3)));
+        } else {
+            verify.accept(verify(analysisResultService, times(1)));
+        }
+    }
+
+    private void testWritingSensitivityValue(boolean batched, boolean throwing) {
+        testWritingValue(
+            () -> resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.),
+            verify -> verify.writeSensitivityValues(any(), anyList()),
+            batched, throwing
+        );
     }
 
     @Test
     void testWritingOneSensitivityValue() {
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
-        resultWriterPersisted.start();
-        assertFalse(resultWriterPersisted.isConsumerFinished());
-
-        resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.);
-        resultWriterPersisted.setQueueProducerFinished();
-        await().atMost(1, TimeUnit.SECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
-        verify(analysisResultService, atLeast(1)).writeSensitivityValues(any(), anyList());
+        testWritingSensitivityValue(false, false);
     }
 
     @Test
     void testWritingSeveralSensitivityValuesIsBatched() {
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
-        resultWriterPersisted.start();
-        assertFalse(resultWriterPersisted.isConsumerFinished());
+        testWritingSensitivityValue(true, false);
+    }
 
-        IntStream.range(0, 1000).forEach(i -> resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.));
-        resultWriterPersisted.setQueueProducerFinished();
-
-        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
-        verify(analysisResultService, atLeast(2)).writeSensitivityValues(any(), anyList());
+    private void testWritingContingencyValue(boolean batched, boolean throwing) {
+        testWritingValue(
+                () -> resultWriterPersisted.writeContingencyStatus(0, SensitivityAnalysisResult.Status.SUCCESS),
+                verify -> verify.writeContingenciesStatus(any(), anyList()),
+                batched, throwing
+        );
     }
 
     @Test
     void testWritingOneContingencyStatus() {
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
-        resultWriterPersisted.start();
-        assertFalse(resultWriterPersisted.isConsumerFinished());
-
-        resultWriterPersisted.writeContingencyStatus(0, SensitivityAnalysisResult.Status.SUCCESS);
-        resultWriterPersisted.setQueueProducerFinished();
-
-        await().atMost(500, TimeUnit.MILLISECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
-        verify(analysisResultService, atLeast(1)).writeContingenciesStatus(any(), anyList());
+        testWritingContingencyValue(false, false);
     }
 
     @Test
     void testWritingSeveralContingencyStatusesIsBatched() {
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
-        resultWriterPersisted.start();
-        assertFalse(resultWriterPersisted.isConsumerFinished());
-
-        IntStream.range(0, 1000).forEach(i -> resultWriterPersisted.writeContingencyStatus(0, SensitivityAnalysisResult.Status.SUCCESS));
-        resultWriterPersisted.setQueueProducerFinished();
-
-        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
-        verify(analysisResultService, atLeast(2)).writeContingenciesStatus(any(), anyList());
-    }
-
-    @Test
-    void testNotOperatingAfterInterruption() throws InterruptedException {
-        resultWriterPersisted.start();
-        resultWriterPersisted.interrupt();
-        Thread.sleep(500);
-        resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.);
-        Thread.sleep(500);
-        assertTrue(resultWriterPersisted.isConsumerFinished());
-        verify(analysisResultService, times(0)).writeSensitivityValues(any(), anyList());
+        testWritingContingencyValue(true, false);
     }
 
     @Test
     void testIsEndingIfErrorOccursPersistingSensitivityValues() {
-        doThrow(new RuntimeException("Error persisting sensitivity values"))
-            .when(analysisResultService)
-            .writeSensitivityValues(any(), anyList());
-        resultWriterPersisted.start();
-        IntStream.range(0, 1000).forEach(i -> resultWriterPersisted.writeSensitivityValue(0, 0, 0., 0.));
-        resultWriterPersisted.setQueueProducerFinished();
-        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
+        testWritingSensitivityValue(false, true);
     }
 
     @Test
     void testIsEndingIfErrorOccursPersistingContingencyStatuses() {
-        doThrow(new RuntimeException("Error persisting contingency statuses"))
-            .when(analysisResultService)
-            .writeContingenciesStatus(any(), anyList());
-        resultWriterPersisted.start();
-        IntStream.range(0, 1000).forEach(i -> resultWriterPersisted.writeContingencyStatus(0, SensitivityAnalysisResult.Status.SUCCESS));
-        resultWriterPersisted.setQueueProducerFinished();
-        await().atMost(1000, TimeUnit.MILLISECONDS).until(() -> resultWriterPersisted.isConsumerFinished());
+        testWritingContingencyValue(false, true);
     }
 }
