4a991bc4a042b7073d98f43f5acd3bc8c474ba7d
carojeandat <112859659+carojeandat@users.noreply.github.com>
2026-06-01 17:08:02 +0200

    Update src/main/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisController.java
    Co-authored-by: Thang PHAM <117309322+thangqp@users.noreply.github.com>

--- src/main/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisController.java
+++ src/main/java/org/gridsuite/sensitivityanalysis/server/SensitivityAnalysisController.java
@@ -110,7 +110,7 @@ public class SensitivityAnalysisController {
                                            @Parameter(description = "The type name for the report") @RequestParam(name = "reportType", required = false, defaultValue = "SensitivityAnalysis") String reportType,
                                            @Parameter(description = "parametersUuid") @RequestParam(name = "parametersUuid", required = false) UUID parametersUuid,
                                            @Parameter(description = "loadFlow parameters uuid") @RequestParam(name = "loadFlowParametersUuid") UUID loadFlowParametersUuid,
-                                           @Parameter(description = "names of the contingency lists and filters contained in the parameters") @RequestBody Map<UUID, String> elementsIdNameMap,
+                                           @Parameter(description = "names of the contingency lists and filters contained in the parameters") @RequestBody(required = false) Map<UUID, String> elementsIdNameMap,
                                            @RequestHeader(HEADER_USER_ID) String userId) {
         SensitivityAnalysisRunContext runContext = sensitivityAnalysisParametersService.createRunContext(
                 networkUuid,
